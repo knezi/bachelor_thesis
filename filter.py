ca04ddbae8c5c5b13a1fec19213c2d73378f79bf
Jan Knížek <jk@janknizek.cz>
2018-04-27 19:07:08 +0200

    filter data

---
+++ filter.py
@@ -0,0 +1,34 @@
+#!/bin/env python3
+import json
+import datetime as dt
+import sys
+
+
+min_date=dt.datetime(2100,1,1)
+max_date=dt.datetime(100,1,1)
+from_date=dt.datetime(2012, 5, 1) # first half of may
+to_date=dt.datetime(2012, 5, 15)
+count=0
+
+try:
+    with open(sys.argv[1], 'w') as w:
+        while True:
+            d=json.loads(input().strip())
+            date=dt.datetime(*[int(x) for x in d['date'].split("-")])
+            min_date=min(date, min_date)
+            max_date=max(date, max_date)
+            
+            if from_date <= date <= to_date:
+                count+=1
+                w.write("{}\n".format(d))
+
+            if count>=20000:
+                break
+
+
+except EOFError as e:
+    pass
+
+print(min_date)
+print(max_date)
+print("{} lines write from {} to {}.".format(count, from_date, to_date))
