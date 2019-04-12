5d6665b4211b14cb717e9fa1b8ab975a08a2ddba
Jan Knížek <jk@janknizek.cz>
2019-04-12 18:49:39 +1000

    stash

--- crop_geneea.py
+++ crop_geneea.py
@@ -6,7 +6,6 @@ It takes geenea file, extracts from it only used data
 (sentiment, entities and id) and saves to another file."""
 import sys
 import json
-from geneea.analyzer.model import f2converter
 import argparse
 
 
@@ -15,19 +14,15 @@ def main(args: argparse.ArgumentParser) -> None:
     with open(args.in_file, 'r', encoding='utf8') as analysisReader, \
             open(args.out_file, 'w', encoding='utf-8') as w:
 
-        # each line is parsed as json and then
-        # loaded with f2converter
+        used_features = ('sentiment', 'id', 'entities')
+        # each line is parsed as json and then cropped
+        # there's no need to load it with geneea analyzer
         for raw in map(json.loads, analysisReader):
-            analysis = f2converter.fromDict(raw)
-
-            # new cropped line of a file
+            # new cropped line
             line = dict()
-            line['sentiment'] = analysis.sentiment.label \
-                if analysis.sentiment is not None else 'n/a'
-            line['id'] = analysis.docId
-            # we have to convert entities from and to dict to avoid
-            # parsing errors and json encoding
-            line['entities'] = f2converter.toDict(analysis)['entities']
+            for f in used_features:
+                if f in raw:
+                    line[f] = raw[f]
 
             w.write(f'{json.dumps(line)}\n')
 
